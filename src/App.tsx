8e087ea999ad4ec42ef65241f274d5d05dfc57de
Jiten Dhull <148058527+Jaggu762@users.noreply.github.com>
2025-10-02 13:31:38 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Services />
       <Projects />
+      <Contact />
     </div>
   );
 }
