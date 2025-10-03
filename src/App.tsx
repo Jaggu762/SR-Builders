ce3a8e8af9fc4236448f481086de13c893b8dc6d
Jiten Dhull <148058527+Jaggu762@users.noreply.github.com>
2025-10-03 17:29:16 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <About />
       <Services />
       <Projects />
-      <Contact />
     </div>
   );
 }
